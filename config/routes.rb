060afe3c2d0936e9e54002983d820dea4338cb91
Gleds Strublic <gc.strublic@gmail.com>
2023-06-08 19:45:16 -0300

    Add root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'recipes/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "recipes#index"
+  resources :recipes
 end
