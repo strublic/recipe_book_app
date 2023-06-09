8dc22a095504df20a4a13192ee0411b714f71e05
Gleds Strublic <gc.strublic@gmail.com>
2023-06-09 09:45:31 -0300

    Add seeds for DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,54 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Seeding...'
+
+puts 'Creating 10 Recipes ...'
+
+10.times do
+    Recipe.create!(
+        name: Faker::Movies::HarryPotter.character,
+        body: Faker::Food.description,
+        ingredients:[
+            Ingredient.new(
+                name: Faker::Food.ingredient,
+                measure: rand(1..30),
+                measure_type: ["ml","litro","grama","kg"].sample
+            ),
+            Ingredient.new(
+                name: Faker::Food.ingredient,
+                measure: rand(1..30),
+                measure_type: ["ml","litro","grama","kg"].sample
+            ),
+            Ingredient.new(
+                name: Faker::Food.ingredient,
+                measure: rand(1..30),
+                measure_type: ["ml","litro","grama","kg"].sample
+            ),
+            Ingredient.new(
+                name: Faker::Food.ingredient,
+                measure: rand(1..30),
+                measure_type: ["ml","litro","grama","kg"].sample
+            )
+        ],
+        method_steps:[
+            MethodStep.new(
+                step: Faker::Lorem.word
+            ),
+            MethodStep.new(
+                step: Faker::Lorem.word
+            ),
+            MethodStep.new(
+                step: Faker::Lorem.word
+            ),
+            MethodStep.new(
+                step: Faker::Lorem.word
+            ),
+            MethodStep.new(
+                step: Faker::Lorem.word
+            ),
+        ]
+    )
+
+puts ">>> done"
